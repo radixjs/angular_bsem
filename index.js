d640ca2c2de1d650c94e45775ef10a8ac212ebad
Thrivaldi <contact@cortneyknorr.com>
2017-03-31 22:43:33 +0200

    Child process

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ function mdir(path) {
 
 function execute(command) {
     return new Promise(function (resolve, reject) {
-        var pro = process.exec(command, function (error, stdout, stderr) {
+        var pro = require('child_process').exec(command, function (error, stdout, stderr) {
             resolve({
                 error,
                 stdout,
