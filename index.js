8bc75ee7e2de18519af68e0d7127b3821eca78b4
Thrivaldi <contact@cortneyknorr.com>
2017-04-21 20:22:37 +0200

    i

--- index.js
+++ index.js
@@ -379,7 +379,8 @@ function generateController(mod){
     if(!mod.settings.name){
         throw "No name specified please specify using key word 'called'";
     }
-    console.log(require("front/javascript/"+mod.settings.source).controllers);
+    console.log(require("./front/javascript/"+mod.settings.source).controllers);
+    console.log()
 }
 function generateApp(mod) {
 
